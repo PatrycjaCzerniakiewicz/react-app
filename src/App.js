4cdff57fdc47b21470945f849a613e0fdbc3677a
unknown <you@example.com>
2021-01-26 13:52:30 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -2,20 +2,39 @@ import React from 'react'
 import './App.css';
 import Nav from './components/Nav/Nav';
 import HeroFeatured from './components/HeroFeatured/HeroFeatured';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
 
 function App() {
   
   return (
     <>
-      <Nav></Nav>
+    <Router>
+      <Nav />
       <main>
         <div className="container">
-          <HeroFeatured />
+          <Switch>
+            <Route exact path="/">
+              <HeroFeatured />
+            </Route>
+            <Route path="/search:name">
+
+            </Route>
+            <Route path="/hero/:id">
+
+            </Route>
+          </Switch> 
         </div> 
       </main>
       <footer>
+        <div className="container">
+          <p>This content is kindly provided by <a href="https://superheroapi.com/">Superhero API</a></p>
+        </div>
       </footer>
-
+    </Router>
     </>
   );
 }
